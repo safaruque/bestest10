baf7c33c93be7bf56925a0610375b824634721b8
Safique Ahmed Faruque <safaruque1@gmail.com>
2015-03-07 11:29:04 +0800

    Fix Header

--- shakib-al-hasan/js/main.js
+++ shakib-al-hasan/js/main.js
@@ -25,15 +25,15 @@ $(document).ready(function () {
 
   /* Header Height Control
    ========================================================================== */
-  var height = $(window).height();
-  if(height<600) {
-    height = 600;
-  }
-  $('header').css({
-    'minHeight': 0,
-    'maxHeight': 'none',
-    'height': height
-  });
+  //var height = $(window).height();
+  //if(height<600) {
+  //  height = 600;
+  //}
+  //$('header').css({
+  //  'minHeight': 0,
+  //  'maxHeight': 'none',
+  //  'height': height
+  //});
   /* ========================================================================
    Header carousel
    ========================================================================== */
@@ -131,7 +131,7 @@ $(document).ready(function () {
       html += '</p>';
       element.innerHTML = html;
   }
-  twitterFetcher.fetch(config);
+  //twitterFetcher.fetch(config);
 
 
   /* ========================================================================
@@ -202,27 +202,27 @@ $(document).ready(function () {
   /* ========================================================================
      Component: Map
    ========================================================================== */
-  google.maps.event.addDomListener(window, 'load', init);
-
-  function init() {
-    var myLatlng = new google.maps.LatLng(41.850033, -87.6500523);
-    var mapOptions = {
-        zoom: 15,
-        scrollwheel: false,
-        navigationControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-        draggable: true,
-        center: myLatlng,
-        styles: [{featureType:"landscape",stylers:[{saturation:-100},{lightness:65},{visibility:"on"}]},{featureType:"poi",stylers:[{saturation:-100},{lightness:51},{visibility:"simplified"}]},{featureType:"road.highway",stylers:[{saturation:-100},{visibility:"simplified"}]},{featureType:"road.arterial",stylers:[{saturation:-100},{lightness:30},{visibility:"on"}]},{featureType:"road.local",stylers:[{saturation:-100},{lightness:40},{visibility:"on"}]},{featureType:"transit",stylers:[{saturation:-100},{visibility:"simplified"}]},{featureType:"administrative.province",stylers:[{visibility:"off"}]/**/},{featureType:"administrative.locality",stylers:[{visibility:"off"}]},{featureType:"administrative.neighborhood",stylers:[{visibility:"on"}]/**/},{featureType:"water",elementType:"labels",stylers:[{visibility:"on"},{lightness:-25},{saturation:-100}]},{featureType:"water",elementType:"geometry",stylers:[{hue:"#ffff00"},{lightness:-25},{saturation:-97}]}],
-    };
-    var mapElement = document.getElementById('map-container');
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        title: 'KreFolio!'
-    });
-  }
+  //google.maps.event.addDomListener(window, 'load', init);
+  //
+  //function init() {
+  //  var myLatlng = new google.maps.LatLng(41.850033, -87.6500523);
+  //  var mapOptions = {
+  //      zoom: 15,
+  //      scrollwheel: false,
+  //      navigationControl: false,
+  //      mapTypeControl: false,
+  //      scaleControl: false,
+  //      draggable: true,
+  //      center: myLatlng,
+  //      styles: [{featureType:"landscape",stylers:[{saturation:-100},{lightness:65},{visibility:"on"}]},{featureType:"poi",stylers:[{saturation:-100},{lightness:51},{visibility:"simplified"}]},{featureType:"road.highway",stylers:[{saturation:-100},{visibility:"simplified"}]},{featureType:"road.arterial",stylers:[{saturation:-100},{lightness:30},{visibility:"on"}]},{featureType:"road.local",stylers:[{saturation:-100},{lightness:40},{visibility:"on"}]},{featureType:"transit",stylers:[{saturation:-100},{visibility:"simplified"}]},{featureType:"administrative.province",stylers:[{visibility:"off"}]/**/},{featureType:"administrative.locality",stylers:[{visibility:"off"}]},{featureType:"administrative.neighborhood",stylers:[{visibility:"on"}]/**/},{featureType:"water",elementType:"labels",stylers:[{visibility:"on"},{lightness:-25},{saturation:-100}]},{featureType:"water",elementType:"geometry",stylers:[{hue:"#ffff00"},{lightness:-25},{saturation:-97}]}],
+  //  };
+  //  var mapElement = document.getElementById('map-container');
+  //  var map = new google.maps.Map(mapElement, mapOptions);
+  //
+  //  var marker = new google.maps.Marker({
+  //      position: myLatlng,
+  //      map: map,
+  //      title: 'KreFolio!'
+  //  });
+  //}
 });
