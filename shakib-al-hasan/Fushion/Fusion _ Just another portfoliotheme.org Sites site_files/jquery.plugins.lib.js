5ac79d5ab88bfcd550cec747b521fe6b756fd7d5
zakera11 <zakera.wadud@gmail.com>
2015-05-20 05:05:24 +0800

    psd modified

---
+++ shakib-al-hasan/Fushion/Fusion _ Just another portfoliotheme.org Sites site_files/jquery.plugins.lib.js
@@ -0,0 +1,401 @@
+/***********
+ Animates element's number to new number with commas
+ Parameters:
+ commas (boolean): turn commas on/off (default is true)
+ duration (number): how long in ms (default is 1000)
+ ease (string): type of easing (default is "swing", others are avaiable from jQuery's easing plugin
+ Examples:
+ $("#div").animateNumbers(false, 500, "linear"); // half second linear without commas
+ $("#div").animateNumbers(true, 2000); // two second swing with commas
+ $("#div").animateNumbers(); // one second swing with commas
+ This fully expects an element containing an integer and with a attribute named data-stop specifying the number at which the stats needs to stop
+ If the number is within copy then separate it with a span and target the span
+ Inserts and accounts for commas during animation by default
+ ***********/
+
+(function($) {
+    $.fn.animateNumbers = function(commas, duration, ease) {
+        return this.each(function() {
+            var $this = $(this);
+            var start = parseInt($this.text().replace(/,/g, ""));
+            var stop = parseInt($this.attr('data-stop').replace(/,/g, ""));
+            commas = (commas === undefined) ? true : commas;
+            $({value: start}).animate({value: stop}, {
+                duration: duration == undefined ? 1000 : duration,
+                easing: ease == undefined ? "swing" : ease,
+                step: function() {
+                    $this.text(Math.floor(this.value));
+                    if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
+                },
+                complete: function() {
+                    if (parseInt($this.text()) !== stop) {
+                        $this.text(stop);
+                        if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
+                    }
+                }
+            });
+        });
+    };
+})(jQuery);
+
+
+
+/**!
+ * easyPieChart
+ * Lightweight plugin to render simple, animated and retina optimized pie charts
+ *
+ * @license Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+ * @author Robert Fleischmann <rendro87@gmail.com> (http://robert-fleischmann.de)
+ * @version 2.1.1
+ **/
+!function(a,b){"object"==typeof exports?module.exports=b(require("jquery")):"function"==typeof define&&define.amd?define("EasyPieChart",["jquery"],b):b(a.jQuery)}(this,function(a){var b=function(a,b){var c,d=document.createElement("canvas");"undefined"!=typeof G_vmlCanvasManager&&G_vmlCanvasManager.initElement(d);var e=d.getContext("2d");d.width=d.height=b.size,a.appendChild(d);var f=1;window.devicePixelRatio>1&&(f=window.devicePixelRatio,d.style.width=d.style.height=[b.size,"px"].join(""),d.width=d.height=b.size*f,e.scale(f,f)),e.translate(b.size/2,b.size/2),e.rotate((-0.5+b.rotate/180)*Math.PI);var g=(b.size-b.lineWidth)/2;b.scaleColor&&b.scaleLength&&(g-=b.scaleLength+2),Date.now=Date.now||function(){return+new Date};var h=function(a,b,c){c=Math.min(Math.max(0,c||1),1),e.beginPath(),e.arc(0,0,g,0,2*Math.PI*c,!1),e.strokeStyle=a,e.lineWidth=b,e.stroke()},i=function(){var a,c,d=24;e.lineWidth=1,e.fillStyle=b.scaleColor,e.save();for(var d=24;d>0;--d)0===d%6?(c=b.scaleLength,a=0):(c=.6*b.scaleLength,a=b.scaleLength-c),e.fillRect(-b.size/2+a,0,c,1),e.rotate(Math.PI/12);e.restore()},j=function(){return window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||function(a){window.setTimeout(a,1e3/60)}}(),k=function(){b.scaleColor&&i(),b.trackColor&&h(b.trackColor,b.lineWidth)};this.clear=function(){e.clearRect(b.size/-2,b.size/-2,b.size,b.size)},this.draw=function(a){b.scaleColor||b.trackColor?e.getImageData&&e.putImageData?c?e.putImageData(c,0,0):(k(),c=e.getImageData(0,0,b.size*f,b.size*f)):(this.clear(),k()):this.clear(),e.lineCap=b.lineCap;var d;d="function"==typeof b.barColor?b.barColor(a):b.barColor,a>0&&h(d,b.lineWidth,a/100)}.bind(this),this.animate=function(a,c){var d=Date.now();b.onStart(a,c);var e=function(){var f=Math.min(Date.now()-d,b.animate),g=b.easing(this,f,a,c-a,b.animate);this.draw(g),b.onStep(a,c,g),f>=b.animate?b.onStop(a,c):j(e)}.bind(this);j(e)}.bind(this)},c=function(a,c){var d={barColor:"#ef1e25",trackColor:"#f9f9f9",scaleColor:"#dfe0e0",scaleLength:5,lineCap:"round",lineWidth:3,size:110,rotate:0,animate:1e3,easing:function(a,b,c,d,e){return b/=e/2,1>b?d/2*b*b+c:-d/2*(--b*(b-2)-1)+c},onStart:function(){},onStep:function(){},onStop:function(){}};if("undefined"!=typeof b)d.renderer=b;else{if("undefined"==typeof SVGRenderer)throw new Error("Please load either the SVG- or the CanvasRenderer");d.renderer=SVGRenderer}var e={},f=0,g=function(){this.el=a,this.options=e;for(var b in d)d.hasOwnProperty(b)&&(e[b]=c&&"undefined"!=typeof c[b]?c[b]:d[b],"function"==typeof e[b]&&(e[b]=e[b].bind(this)));e.easing="string"==typeof e.easing&&"undefined"!=typeof jQuery&&jQuery.isFunction(jQuery.easing[e.easing])?jQuery.easing[e.easing]:d.easing,this.renderer=new e.renderer(a,e),this.renderer.draw(f),a.dataset&&a.dataset.percent?this.update(parseFloat(a.dataset.percent)):a.getAttribute&&a.getAttribute("data-percent")&&this.update(parseFloat(a.getAttribute("data-percent")))}.bind(this);this.update=function(a){return a=parseFloat(a),e.animate?this.renderer.animate(f,a):this.renderer.draw(a),f=a,this}.bind(this),g()};a.fn.easyPieChart=function(b){return this.each(function(){a.data(this,"easyPieChart")||a.data(this,"easyPieChart",new c(this,b))})}});
+
+
+/*global jQuery */
+/*!
+ * FitVids 1.0
+ *
+ * Copyright 2011, Chris Coyier - http://css-tricks.com + Dave Rupert - http://daverupert.com
+ * Credit to Thierry Koblentz - http://www.alistapart.com/articles/creating-intrinsic-ratios-for-video/
+ * Released under the WTFPL license - http://sam.zoy.org/wtfpl/
+ *
+ * Date: Thu Sept 01 18:00:00 2011 -0500
+ */
+
+(function( $ ){
+
+    $.fn.fitVids = function( options ) {
+        var settings = {
+            customSelector: null
+        }
+
+        var div = document.createElement('div'),
+            ref = document.getElementsByTagName('base')[0] || document.getElementsByTagName('script')[0];
+
+        div.className = 'fit-vids-style';
+        div.innerHTML = '&shy;<style>         \
+      .fluid-width-video-wrapper {        \
+         width: 100%;                     \
+         position: relative;              \
+         padding: 0;                      \
+      }                                   \
+                                          \
+      .fluid-width-video-wrapper iframe,  \
+      .fluid-width-video-wrapper object,  \
+      .fluid-width-video-wrapper embed {  \
+         position: absolute;              \
+         top: 0;                          \
+         left: 0;                         \
+         width: 100%;                     \
+         height: 100%;                    \
+      }                                   \
+    </style>';
+
+        ref.parentNode.insertBefore(div,ref);
+
+        if ( options ) {
+            $.extend( settings, options );
+        }
+
+        return this.each(function(){
+            var selectors = [
+                "iframe[src*='player.vimeo.com']",
+                "iframe[src*='www.youtube.com']",
+                "iframe[src*='www.kickstarter.com']",
+                "object",
+                "embed"
+            ];
+
+            if (settings.customSelector) {
+                selectors.push(settings.customSelector);
+            }
+
+            var $allVideos = $(this).find(selectors.join(','));
+
+            $allVideos.each(function(){
+                var $this = $(this);
+                if (this.tagName.toLowerCase() == 'embed' && $this.parent('object').length || $this.parent('.fluid-width-video-wrapper').length) { return; }
+                var height = ( this.tagName.toLowerCase() == 'object' || $this.attr('height') ) ? $this.attr('height') : $this.height(),
+                    width = $this.attr('width') ? $this.attr('width') : $this.width(),
+                    aspectRatio = height / width;
+                if(!$this.attr('id')){
+                    var videoID = 'fitvid' + Math.floor(Math.random()*999999);
+                    $this.attr('id', videoID);
+                }
+                $this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', (aspectRatio * 100)+"%");
+                $this.removeAttr('height').removeAttr('width');
+            });
+        });
+    }
+})( jQuery );
+
+
+/*!
+ * jQuery Smooth Scroll - v1.5.3 - 2014-10-15
+ * https://github.com/kswedberg/jquery-smooth-scroll
+ * Copyright (c) 2014 Karl Swedberg
+ * Licensed MIT (https://github.com/kswedberg/jquery-smooth-scroll/blob/master/LICENSE-MIT)
+ */
+
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+
+    var version = '1.5.3',
+        optionOverrides = {},
+        defaults = {
+            exclude: [],
+            excludeWithin:[],
+            offset: 0,
+
+            // one of 'top' or 'left'
+            direction: 'top',
+
+            // jQuery set of elements you wish to scroll (for $.smoothScroll).
+            //  if null (default), $('html, body').firstScrollable() is used.
+            scrollElement: null,
+
+            // only use if you want to override default behavior
+            scrollTarget: null,
+
+            // fn(opts) function to be called before scrolling occurs.
+            // `this` is the element(s) being scrolled
+            beforeScroll: function() {},
+
+            // fn(opts) function to be called after scrolling occurs.
+            // `this` is the triggering element
+            afterScroll: function() {},
+            easing: 'swing',
+            speed: 400,
+
+            // coefficient for "auto" speed
+            autoCoefficient: 2,
+
+            // $.fn.smoothScroll only: whether to prevent the default click action
+            preventDefault: true
+        },
+
+        getScrollable = function(opts) {
+            var scrollable = [],
+                scrolled = false,
+                dir = opts.dir && opts.dir === 'left' ? 'scrollLeft' : 'scrollTop';
+
+            this.each(function() {
+
+                if (this === document || this === window) { return; }
+                var el = $(this);
+                if ( el[dir]() > 0 ) {
+                    scrollable.push(this);
+                } else {
+                    // if scroll(Top|Left) === 0, nudge the element 1px and see if it moves
+                    el[dir](1);
+                    scrolled = el[dir]() > 0;
+                    if ( scrolled ) {
+                        scrollable.push(this);
+                    }
+                    // then put it back, of course
+                    el[dir](0);
+                }
+            });
+
+            // If no scrollable elements, fall back to <body>,
+            // if it's in the jQuery collection
+            // (doing this because Safari sets scrollTop async,
+            // so can't set it to 1 and immediately get the value.)
+            if (!scrollable.length) {
+                this.each(function() {
+                    if (this.nodeName === 'BODY') {
+                        scrollable = [this];
+                    }
+                });
+            }
+
+            // Use the first scrollable element if we're calling firstScrollable()
+            if ( opts.el === 'first' && scrollable.length > 1 ) {
+                scrollable = [ scrollable[0] ];
+            }
+
+            return scrollable;
+        };
+
+    $.fn.extend({
+        scrollable: function(dir) {
+            var scrl = getScrollable.call(this, {dir: dir});
+            return this.pushStack(scrl);
+        },
+        firstScrollable: function(dir) {
+            var scrl = getScrollable.call(this, {el: 'first', dir: dir});
+            return this.pushStack(scrl);
+        },
+
+        smoothScroll: function(options, extra) {
+            options = options || {};
+
+            if ( options === 'options' ) {
+                if ( !extra ) {
+                    return this.first().data('ssOpts');
+                }
+                return this.each(function() {
+                    var $this = $(this),
+                        opts = $.extend($this.data('ssOpts') || {}, extra);
+
+                    $(this).data('ssOpts', opts);
+                });
+            }
+
+            var opts = $.extend({}, $.fn.smoothScroll.defaults, options),
+                locationPath = $.smoothScroll.filterPath(location.pathname);
+
+            this
+                .unbind('click.smoothscroll')
+                .bind('click.smoothscroll', function(event) {
+                    var link = this,
+                        $link = $(this),
+                        thisOpts = $.extend({}, opts, $link.data('ssOpts') || {}),
+                        exclude = opts.exclude,
+                        excludeWithin = thisOpts.excludeWithin,
+                        elCounter = 0, ewlCounter = 0,
+                        include = true,
+                        clickOpts = {},
+                        hostMatch = ((location.hostname === link.hostname) || !link.hostname),
+                        pathMatch = thisOpts.scrollTarget || ( $.smoothScroll.filterPath(link.pathname) === locationPath ),
+                        thisHash = escapeSelector(link.hash);
+
+                    if ( !thisOpts.scrollTarget && (!hostMatch || !pathMatch || !thisHash) ) {
+                        include = false;
+                    } else {
+                        while (include && elCounter < exclude.length) {
+                            if ($link.is(escapeSelector(exclude[elCounter++]))) {
+                                include = false;
+                            }
+                        }
+                        while ( include && ewlCounter < excludeWithin.length ) {
+                            if ($link.closest(excludeWithin[ewlCounter++]).length) {
+                                include = false;
+                            }
+                        }
+                    }
+
+                    if ( include ) {
+
+                        if ( thisOpts.preventDefault ) {
+                            event.preventDefault();
+                        }
+
+                        $.extend( clickOpts, thisOpts, {
+                            scrollTarget: thisOpts.scrollTarget || thisHash,
+                            link: link
+                        });
+
+                        $.smoothScroll( clickOpts );
+                    }
+                });
+
+            return this;
+        }
+    });
+
+    $.smoothScroll = function(options, px) {
+        if ( options === 'options' && typeof px === 'object' ) {
+            return $.extend(optionOverrides, px);
+        }
+        var opts, $scroller, scrollTargetOffset, speed, delta,
+            scrollerOffset = 0,
+            offPos = 'offset',
+            scrollDir = 'scrollTop',
+            aniProps = {},
+            aniOpts = {};
+
+        if (typeof options === 'number') {
+            opts = $.extend({link: null}, $.fn.smoothScroll.defaults, optionOverrides);
+            scrollTargetOffset = options;
+        } else {
+            opts = $.extend({link: null}, $.fn.smoothScroll.defaults, options || {}, optionOverrides);
+            if (opts.scrollElement) {
+                offPos = 'position';
+                if (opts.scrollElement.css('position') === 'static') {
+                    opts.scrollElement.css('position', 'relative');
+                }
+            }
+        }
+
+        scrollDir = opts.direction === 'left' ? 'scrollLeft' : scrollDir;
+
+        if ( opts.scrollElement ) {
+            $scroller = opts.scrollElement;
+            if ( !(/^(?:HTML|BODY)$/).test($scroller[0].nodeName) ) {
+                scrollerOffset = $scroller[scrollDir]();
+            }
+        } else {
+            $scroller = $('html, body').firstScrollable(opts.direction);
+        }
+
+        // beforeScroll callback function must fire before calculating offset
+        opts.beforeScroll.call($scroller, opts);
+
+        scrollTargetOffset = (typeof options === 'number') ? options :
+            px ||
+                ( $(opts.scrollTarget)[offPos]() &&
+                    $(opts.scrollTarget)[offPos]()[opts.direction] ) ||
+                0;
+
+        aniProps[scrollDir] = scrollTargetOffset + scrollerOffset + opts.offset;
+        speed = opts.speed;
+
+        // automatically calculate the speed of the scroll based on distance / coefficient
+        if (speed === 'auto') {
+
+            // $scroller.scrollTop() is position before scroll, aniProps[scrollDir] is position after
+            // When delta is greater, speed will be greater.
+            delta = aniProps[scrollDir] - $scroller.scrollTop();
+            if(delta < 0) {
+                delta *= -1;
+            }
+
+            // Divide the delta by the coefficient
+            speed = delta / opts.autoCoefficient;
+        }
+
+        aniOpts = {
+            duration: speed,
+            easing: opts.easing,
+            complete: function() {
+                opts.afterScroll.call(opts.link, opts);
+            }
+        };
+
+        if (opts.step) {
+            aniOpts.step = opts.step;
+        }
+
+        if ($scroller.length) {
+            $scroller.stop().animate(aniProps, aniOpts);
+        } else {
+            opts.afterScroll.call(opts.link, opts);
+        }
+    };
+
+    $.smoothScroll.version = version;
+    $.smoothScroll.filterPath = function(string) {
+        string = string || '';
+        return string
+            .replace(/^\//,'')
+            .replace(/(?:index|default).[a-zA-Z]{3,4}$/,'')
+            .replace(/\/$/,'');
+    };
+
+    // default options
+    $.fn.smoothScroll.defaults = defaults;
+
+    function escapeSelector (str) {
+        return str.replace(/(:|\.)/g,'\\$1');
+    }
+
+}));
+
---
+++ shakib-al-hasan/Fushion/Fusion _ Just another portfoliotheme.org Sites site_files/skin.php
@@ -0,0 +1,107 @@
+
+/* ------- The links --------- */
+a, a:active, a:visited { color: #e84a52; }
+a:hover { color: #888; }
+.sec-nav a, .sec-nav li > a { color: #d3d3d1; }
+.sidebar li > a:hover, .sidebar li:hover > a { color: #e84a52; }
+
+blockquote .author, blockquote em, blockquote i, blockquote cite { color: #e84a52; }
+.page-links a, .page-links a:visited, .pagination a, .pagination a:visited { background-color: #e84a52; }
+
+.dropdown-menu-wrap ul.sub-menu li:hover > a { color: #e84a52;}
+.dropdown-menu-wrap > ul.menu > li.active { border-color: #e84a52; }
+.dropdown-menu-wrap > ul.menu > li:hover { border-color: #e84a52; }
+.dropdown-menu-wrap > ul.menu > li.sfHover { border-color: transparent ; }
+#primary-menu > ul.menu > li > a:hover { color: #e84a52; }
+
+#title-area { background-color: #e84a52; }
+#custom-title-area { background-color: #e84a52; }
+.post-list .entry-title a:hover, .post-snippets .hentry .entry-title a:hover { color: #e84a52; }
+#content .hentry .entry-meta a:hover { color: #e84a52; }
+.sticky .entry-snippet { border-color: #e84a52;}
+.post-snippets .byline a:hover { color: #e84a52;}
+.byline span i { color: #e84a52;}
+a.more-link:hover { color: #e84a52; }
+a.comment-reply-link, a.comment-edit-link { background-color: #e84a52; }
+a.comment-reply-link:visited, a.comment-edit-link:visited { background-color: #e84a52; }
+
+.top-of-page a:hover, .post-list .byline a, .post-list .byline a:active, .post-list .byline a:visited,
+#content .hentry h2.entry-title a:hover, .entry-meta span i, .read-more a, .loop-nav a:hover { color: #e84a52; }
+button, .button, input[type=button], input[type="submit"], input[type="reset"] { background-color: #e84a52; border-color: #e84a52;}
+.button.theme:hover { background: #e84a52 !important; border-color: #e84a52 !important; }
+.button.theme { border-color: #e84a52; }
+
+#flickr-widget .flickr_badge_image img:hover { border-color: #e84a52; }
+ul#recentcomments li.recentcomments a { color: #e84a52; }
+.tagcloud a:hover { background-color: #e84a52; }
+input#mc_signup_submit { background-color: #e84a52 !important; }
+
+.header-fancy span { background-color: #e84a52; }
+h3.fancy-header { background-color: #e84a52;}
+
+.heading .title.dashed:after { background-color: #e84a52; }
+
+.segment.slogan blockquote .footer cite { color: #e84a52; }
+.portfolio-label { color: #e84a52; }
+#showcase-filter a:hover, #showcase-filter a.active, #showcase-links a:hover, #showcase-links a.active { background: #e84a52; border-color: #e84a52; }
+
+.stats-bar-content { background: #e84a52; }
+.animate-numbers .stats .number { color: #e84a52;}
+
+.pricing-table .pricing-plan.highlight .top-header h3 { color: #e84a52 !important; }
+
+.testimonials2-slider-container blockquote cite i { color: #e84a52; }
+.client-testimonials2 .header cite { color: #e84a52;}
+.testimonials-slider-container cite .client-name { color: #e84a52; }
+.app-benefit i { color: #e84a52;}
+.member-profile h3:after { background-color: #e84a52; }
+#showcase-filter a:hover { background: #e84a52; border-color: #e84a52; }
+.featured-items h4:after { background-color: #e84a52; }
+
+#column-shortcode-section p { background: #e84a52; }
+
+
+#sidebars-footer .widget_text a.small, #sidebars-footer .widget_text a.small:visited,
+#home-intro h2 span { color: #e84a52; }
+#sidebar-header .social-list a:hover i { color: #e84a52; }
+
+.image-info-buttons i, .image-info h3 a:hover, .image-info .terms a:hover { color: #e84a52; }
+input:focus, textarea:focus, #content .contact-form input:focus, #content .contact-form textarea:focus,
+.sec-nav .contact-form input:focus, .sec-nav .contact-form textarea:focus { border-color: rgba(232, 74 , 82, 0.8); }
+#home2-heading .heading2 h2, #home3-heading .heading2 h2 { background: rgba(232, 74 , 82, 0.7); }
+
+.sec-nav .button:hover, .sec-nav button:hover, .sec-nav input[type="button"]:hover, .sec-nav input[type="submit"]:hover, .sec-nav input[type="reset"]:hover {
+background-color: #e84a52 !important;
+border-color: #e84a52 !important;
+}
+.sec-nav a:hover, .sec-nav li > a:hover { color: #e84a52; }
+
+.tabs .current, .tabs .current:hover, .tabs li.current a { border-top-color: #e84a52; }
+
+ul.tab-list { border-bottom: 1px solid #e84a52; }
+ul.tab-list li a.visible, ul.tab-list li a.flex-active { border-bottom: 3px solid #e84a52; }
+ul.tab-list li a:hover { color: #e84a52; }
+#features-slider ul.tab-list li a.visible i, #features-slider ul.tab-list li a.visible span,
+#features-slider ul.tab-list li a.flex-active i, #features-slider ul.tab-list li a.flex-active span,
+#features-slider2 ul.tab-list li a.flex-active i, #features-slider2 ul.tab-list li a.flex-active span { color: #e84a52; }
+
+#services-slider ul.tab-list li a.visible i, #services-slider ul.tab-list li a.visible span,
+#services-slider ul.tab-list li a.flex-active i, #services-slider ul.tab-list li a.flex-active span { color: #e84a52; }
+
+.team-member:hover h3 a { color: #e84a52; }
+
+#app-intro .heading .title { color: #e84a52; }
+.app-benefit i, .agency-benefit i { color: #e84a52; }
+#styleswitcher-button i { color: #e84a52; }
+
+/* Plugins Skins Styles */
+
+/* --------- Other Plugins ---------- */
+
+.tp_recent_tweets li a { color: #e84a52 !important; }
+
+.tp-caption.medium_bg_fusion { background-color: #e84a52 !important; }
+
+.widget .instagram-pics img:hover { border-color: #e84a52; }
+
+
